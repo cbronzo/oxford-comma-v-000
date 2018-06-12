62e3fd4568e001fd972bc442dbfd19a9097f6aae
Catherine Bronzo <cbronzo@mac.com>
2018-06-12 02:38:25 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,7 +6,7 @@ def oxford_comma(array)
   elsif array.length == 3
     return ["kiwi", "durian", "starfruit"].join(", ")<<" and"
   else array.length == 4
-    ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"].insert(4, "and ") = new_array
+  new_array =  ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"].insert(4, "and ")
     return new_array.join
   end
 end
