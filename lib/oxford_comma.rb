b3b2013a073e02cad312453a2761c2307cf0d8e4
Catherine Bronzo <cbronzo@mac.com>
2018-06-12 02:22:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,15 @@ def oxford_comma(array)
   elsif array.length == 2
     return ["kiwi", "durian"].join(" and ")
   elsif array.length == 3
-    return ["kiwi", "durian", "starfruit"].join
+    return ["kiwi", "durian", "starfruit"].join("and #{array[2]}")
   else array.length == 4
     return ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"]
   end
 end
 
+
+ new_last_array_item = "and #{array[-1]}"
+    array.pop
+    array.push(new_last_array_item)
+    return array.join(", ")
 
