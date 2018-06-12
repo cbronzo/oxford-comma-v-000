c0b1e439201b1f043e903ed073b6c2d6457892a0
Catherine Bronzo <cbronzo@mac.com>
2018-06-12 02:43:20 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,7 +7,7 @@ def oxford_comma(array)
     return ["kiwi", "durian", "starfruit"].join(", ")<<" and"
   else array.length == 4
     new_array = "and #{array[-1]}"
-    ["kiwi", "durian", "starfruit", "mangos", "dragon fruits"] << new_array
+    array << new_array
     return new_array.join(", ")
   end
 end
